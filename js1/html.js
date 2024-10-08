c8e2b7a81d5a0a14fe92099b5421da7cfd28c674
tuz7777 <136784156+tuz7777@users.noreply.github.com>
2024-10-08 17:10:08 +0300

    Update html.js

--- js1/html.js
+++ js1/html.js
@@ -1,6 +1,6 @@
 // let dlina = Number(prompt('Введите длину от 1 до 10'));
 // let shirina = Number(prompt('Введите ширину от 1 до 10'));
-// if (dlina===""||shirina===""||dlina===0||shirina===0||dlina>10||shirina>10){
+// if (dlina===""||shirina===""||dlina===0||shirina===0||dlina>10||shirina>10||isNaN(dlina)||isNaN(shirina))){
 //     alert('не правилньно');
 // }else if(dlina===shirina){
 //     alert(`Площадь квадрата равна ${dlina*shirina}`);
@@ -17,7 +17,7 @@
 //     alert('Декада 2');
 //  }else if(day<=31){
 //     alert('Декада 3');
-//  }else if(day===0||isNaN(day)){
+//  }else if(day===0||isNaN(day)||day<0){
 //     alert('ошибка')
 //  };
 
@@ -47,21 +47,25 @@
 //     `)
 
 
-let familiy = prompt('Ведите фамилию');
-let my = prompt('Ведите имя');
-let otchestvo = prompt('Ведите отчество');
-let vozrast = prompt('Ведите ваш возраст');
-let pol = confirm('ваш пол мужской?')
-let pensiy = confirm('вы на пенсии?')
+let old = null;
+let name = prompt('Введите ваше имя');
+let lastName = prompt('Введите вашу фамилию');
+let surname = prompt('Введите ваше отчество');
+let howYear = Number(prompt('Введите ваш возраст в годах'));
+let gender = confirm('Ваш пол мужской?');
+gender ? gender = 'Мужской' : gender = 'Женский';
+(gender === 'Мужской' && howYear >= 63) || (gender === 'Женский' && howYear >= 58) ? old = 'Да' :  old = 'Нет';
 
-pol===true ? pol = "Мужской": pol = "Женский" 
-pensiy===true ? pensiy = "да": pensiy = "нет"
- alert (`
-    ваше ФИО ${my} ${familiy} ${otchestvo} 
-    ваш возраст ${vozrast} 
-    через 5 лет вам ${my}  
-    ваш пол: ${pol}  
-    вы на пенсии ${pensiy}
-    `) 
+
+
+name === null || howYear < 0 || name === '' || lastName === null || lastName === '' || surname === null || surname === '' || howYear === 0 || isNaN(howYear) ?  alert ('Перепроврьте введенные данные.')
+: alert (`
+    ваше ФИО: ${lastName} ${name} ${surname}
+    ваш возраст в годах: ${howYear}
+    ваш возраст в днях: ${(howYear * 365) + (Math.floor(howYear / 4))}
+    через 5 лет вам будет: ${howYear + 5}
+    ваш пол: ${gender}
+    вы на пенсии: ${old}
+`)
 
 
